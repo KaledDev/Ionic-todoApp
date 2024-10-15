5ca7492b4a66649698f9f5723f90af68f30f58f7
KaledDev <sanguissotommy22@gmail.com>
2024-10-15 13:53:57 +0000

    Apprentissage de Ionic avec un projet de todoApp

---
+++ src/app/todo/todo.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.page.html',
+  styleUrls: ['./todo.page.scss'],
+})
+export class TodoPage implements OnInit {
+  tasks: { id: number, title: string, completed: boolean }[] = [];
+  newTask = '';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
