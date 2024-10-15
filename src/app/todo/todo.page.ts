ed149e7e05fa4f02e43564e7feda9016e17495ca
KaledDev <sanguissotommy22@gmail.com>
2024-10-15 14:37:20 +0000

    Projet terminé.

--- src/app/todo/todo.page.ts
+++ src/app/todo/todo.page.ts
@@ -10,6 +10,32 @@ export class TodoPage implements OnInit {
   newTask = '';
   constructor() { }
 
+  //Ajouter une tache
+  addTask() {
+    if (this.newTask.trim().length > 0) {
+      const newTask = {
+        id : Date.now(),
+        title : this.newTask,
+        completed : false
+      };
+      this.tasks.push(newTask);
+      this.newTask = '';
+    }
+  }
+
+  //Supprimer une tache
+    deleteTask(taskId: number) {
+      this.tasks = this.tasks.filter(task => task.id !== taskId);
+    }
+
+  // Marquer une tâche comme terminée
+  toggleTaskCompletion(taskId: number) {
+    const task = this.tasks.find(task => task.id === taskId);
+    if (task) {
+      task.completed = !task.completed;
+    }
+  }
+
   ngOnInit() {
   }
 
